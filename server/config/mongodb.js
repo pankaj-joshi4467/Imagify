a7d6fa38f8101e6cdcfcf5bf026fcc2f7f0e6453
pankaj-joshi4467 <147567394+pankaj-joshi4467@users.noreply.github.com>
2025-01-18 21:44:44 +0530

    Add files via upload

---
+++ server/config/mongodb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB =async ()=>{
+
+    mongoose.connection.on('connected',()=>{
+        console.log('connected to MongoDB');
+    })
+
+
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
+}
+
+export default connectDB;
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {registerUser, loginUser, userCredits, paymentRazorpay} from '../controllers/userController.js'
+import userAuth from '../middlewares/auth.js'
+
+const userRouter =express.Router()
+
+
+
+userRouter.post('/register',registerUser)
+userRouter.post('/login',loginUser)
+userRouter.get('/credits',userAuth,userCredits)
+userRouter.post('/pay-razor',userAuth,paymentRazorpay)
+
+
+export default userRouter ;
+
+
+
+
+
+// http://localhost:4000/api/user/register
+// http://localhost:4000/api/user/login
